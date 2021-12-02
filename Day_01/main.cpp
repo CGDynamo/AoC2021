6200e429a12a64355a7098da4e3dc9832d7a60e4
CGDynamo <30875210+CGDynamo@users.noreply.github.com>
2021-12-02 16:45:58 -0500

    Day 2.1
    Update to include Quality of Life comments

--- Day_01/main.cpp
+++ Day_01/main.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <vector>
 
-
 std::vector<unsigned int> GetDepthsFromInput();
 unsigned int Part1Comparison();
 unsigned int Part2Comparison();
@@ -14,24 +13,38 @@ main()
 	std::cout << Part1Comparison() << ", " << Part2Comparison() << '\n';
 }
 
+/// <summary>
+/// Loads data from formatted input text file
+/// </summary>
+/// <returns> Vector collection of input text as uint32 </returns>
 std::vector<unsigned int> GetDepthsFromInput()
 {
+	//open file
 	std::ifstream file("input.txt", std::ios::in);
 	
+	//generate collection to contain inputs
 	std::vector<unsigned int> inputs;
 	
+	//iterate over each line and pump, as int, into collection. Does not account for flying submarines
 	for(std::string line; std::getline(file, line);)
 		inputs.push_back(std::stoi(line));
 	
 	return inputs;
 }
 
+/// <summary>
+/// Performs the 'part 1' operation
+/// </summary>
+/// <returns> Quantity of depth increases of input instructions </returns>
 unsigned int Part1Comparison()
 {	
+	//get input file values
 	std::vector<unsigned int> depths = GetDepthsFromInput();
 	
+	//counter of terrain depth increments
 	unsigned int deepenings = 0;
 	
+	//increment sea floor depthenings
 	for(int ind = 1; ind < depths.size(); ind++)
 		if (depths[ind] > depths[ind-1])
 			deepenings++;
@@ -39,20 +52,28 @@ unsigned int Part1Comparison()
 	return deepenings;
 }
 
+/// <summary>
+/// Performs the 'part 2' operation
+/// </summary>
+/// <returns> Quantity of depth increases of input instructions, using three consecutive values as samples </returns>
 unsigned int Part2Comparison()
 {	
+	//get input file values
 	std::vector<unsigned int> depths = GetDepthsFromInput();
 	
+	//count of terrain deepenings and sampler of terrain surface
 	unsigned int 
 		deepenings = 0,
 		sample = depths[0] + depths[1] + depths[2];
 	
+	//incrementor of terrain deepenings, smooved over 3-part sample
 	for(int ind = 3; ind < depths.size(); ind++)
 	{
 		if (depths[ind - 2] + depths[ind - 1] + depths[ind] > sample)
 			deepenings++;
 		sample = depths[ind - 2] + depths[ind - 1] + depths[ind];
 	}
+
 	return deepenings;
 }
 
--- Day_02/main.cpp
+++ Day_02/main.cpp
@@ -3,49 +3,69 @@
 #include <vector>
 #include <string>
 
-//
+// enum for directions
 enum Directions{ FORWARD = 'f', DOWN = 'd', UP = 'u' };
 
+// declarations
 std::vector<std::pair<unsigned char, unsigned int>> GetDirectionsFromInput();
-
 void PositionFromDirections(unsigned int& distance, unsigned int& depth, const std::vector<std::pair<unsigned char, unsigned int>>& directions);
 
 main()
 {
+	// get instruction pairs
 	std::vector<std::pair<unsigned char, unsigned int>> directions = GetDirectionsFromInput();
+	// declare values
 	unsigned int distance = 0, depth = 0;
+	// process values from input pairs
 	PositionFromDirections(distance, depth, directions);
+	// output values
 	std::cout << "Traveled: " << distance << ", at a depth of: " << depth << ", for some reason totalling: " << distance * depth << '\n'; 
 }
 
+/// <summary>
+/// Generate collection of pairs from input text file
+/// </summary>
+/// <returns> Collection of instruction pairs </returns>
 std::vector<std::pair<unsigned char, unsigned int>> GetDirectionsFromInput()
 {
+	// open input file
 	std::ifstream file("input.txt", std::ios::in);
+	// generate vector collection of input pairs
 	std::vector<std::pair<unsigned char, unsigned int>> inputs = std::vector<std::pair<unsigned char, unsigned int>>();
 	
+	// generate pairs from each line of input file
 	for(std::string line; std::getline(file, line);)
 	{
 		int carett = line.find(' ');
-		inputs.push_back(std::make_pair(line.at(0), std::stoi(line.substr( carett+1))));
+		inputs.push_back(std::make_pair(line.at(0), std::stoi(line.substr(carett + 1))));
 	}
 	
 	return inputs;
 }
 
+/// <summary>
+/// Calculate horizontal range and descent depth from adjusted pitch, using *really* inaccurate math 
+/// </summary>
+/// <param name="distance"> distance reference </param>
+/// <param name="depth"> depth reference </param>
+/// <param name="directions"> the diving instructions </param>
 void PositionFromDirections(unsigned int& distance, unsigned int& depth, const std::vector<std::pair<unsigned char, unsigned int>>& directions)
 {
+	// submarine trim, "aim" is not a thing related to rotation
 	int trim=0;
+
+	//iterate instructions and process as necessary
 	for(std::pair<unsigned char, unsigned int> direction : directions)
 		switch (direction.first)
 		{
-			case 'f':
+			case FORWARD:
 				distance += direction.second;
 				depth += direction.second * trim;
 				break;
-			case 'u':
+			case UP:
 				trim -= direction.second;
 				break;
-			case 'd':
+			case DOWN:
 				trim += direction.second;
 				break;
 		}
